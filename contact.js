8f084e06c90911eb0968a260a306fe4f6b9240c8
Damsbot64 <damien64durand@gmail.com>
2022-10-05 14:14:16 +0200

    grosse amelioration du js

--- contact.js
+++ contact.js
@@ -13,16 +13,49 @@ link.addEventListener("click", function (def) {
 // Contact them
 
 const button = document.getElementById("buttonSend");
-const inputName = document.getElementById("inputName");
-const inputEmail = document.getElementById("inputEmail")
-const chooseTopic = document.getElementById("chooseTopic")
+const inputEmail = document.getElementById("inputEmail");
+const chooseTopic = document.getElementById("chooseTopic");
+const inputMessage = document.getElementById("inputMessage");
+const firstName = document.getElementById("inputName");
+const inputReturn = document.querySelector(".inputReturn")
 
 
 button.addEventListener("click", function () {
-  if ()
-  const name = document.getElementById("inputName");
-  const yourName = name.value;
-  alert(`Hey ${yourName}, we will get back to you shortly !!!`);
-});
+const yourEmail = inputEmail.value;
+const yourTopic = chooseTopic.value;
+const yourMessage = inputMessage.value;
+const yourName = firstName.value;
 
+if ( yourName === ""){
+  firstName.style.border="solid 1px red"
+  return alert `Please give us your name`
+  }
+  firstName.style.border=""
+if (yourEmail === ""){
+  inputEmail.style.border="solid 1px red"
+  return alert `Please give us your Email address`
+}
+inputEmail.style.border = ""
+if (yourTopic === ""){
+  chooseTopic.style.border="solid 1px red"
+  return alert `Please give us your Topic`
+}
+chooseTopic.style.border= ""
+
+if (yourMessage === ""){
+  inputMessage.style.border="solid 1px red"
+  return alert `Please tell us what you need`
+}
+inputMessage.style.border =""
+
+alert(`hey ${yourName} we will get back to you !!!`)
+
+inputEmail.value = ""
+inputMessage.value = ""
+chooseTopic.value = ""
+firstName.value = ""
+
+
+return 
+});
 
