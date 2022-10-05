9094ef41a33b85dbb7b5b883ccaede55e75c7ea1
Damsbot64 <damien64durand@gmail.com>
2022-10-05 10:24:05 +0200

    Work in progress

--- contact.js
+++ contact.js
@@ -13,8 +13,13 @@ link.addEventListener("click", function (def) {
 // Contact them
 
 const button = document.getElementById("buttonSend");
+const inputName = document.getElementById("inputName");
+const inputEmail = document.getElementById("inputEmail")
+const chooseTopic = document.getElementById("chooseTopic")
+
 
 button.addEventListener("click", function () {
+  if ()
   const name = document.getElementById("inputName");
   const yourName = name.value;
   alert(`Hey ${yourName}, we will get back to you shortly !!!`);
