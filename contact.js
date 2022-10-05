7edc54f5dbe79d06be11a77ce8a54c67e2481956
Damsbot64 <damien64durand@gmail.com>
2022-10-05 10:12:31 +0200

    petits ajustements pour le jset le contact.html

--- contact.js
+++ contact.js
@@ -17,45 +17,42 @@ const inputEmail = document.getElementById("inputEmail");
 const chooseTopic = document.getElementById("chooseTopic");
 const inputMessage = document.getElementById("inputMessage");
 const firstName = document.getElementById("inputName");
-const inputReturn = document.querySelector(".inputReturn")
-
+const inputReturn = document.querySelector(".inputReturn");
 
 button.addEventListener("click", function () {
-const yourEmail = inputEmail.value;
-const yourTopic = chooseTopic.value;
-const yourMessage = inputMessage.value;
-const yourName = firstName.value;
+  const yourEmail = inputEmail.value;
+  const yourTopic = chooseTopic.value;
+  const yourMessage = inputMessage.value;
+  const yourName = firstName.value;
 
-if ( yourName === ""){
-  firstName.style.border="solid 1px red"
-  return alert `Please give us your name`
+  if (yourName === "") {
+    firstName.style.border = "solid 1px red";
+    return alert`Please give us your name`;
   }
-  firstName.style.border=""
-if (yourEmail === ""){
-  inputEmail.style.border="solid 1px red"
-  return alert `Please give us your Email address`
-}
-inputEmail.style.border = ""
-if (yourTopic === ""){
-  chooseTopic.style.border="solid 1px red"
-  return alert `Please give us your Topic`
-}
-chooseTopic.style.border= ""
+  firstName.style.border = "";
+  if (yourEmail === "") {
+    inputEmail.style.border = "solid 1px red";
+    return alert`Please give us your Email address`;
+  }
+  inputEmail.style.border = "";
+  if (yourTopic === "") {
+    chooseTopic.style.border = "solid 1px red";
+    return alert`Please give us your Topic`;
+  }
+  chooseTopic.style.border = "";
 
-if (yourMessage === ""){
-  inputMessage.style.border="solid 1px red"
-  return alert `Please tell us what you need`
-}
-inputMessage.style.border =""
+  if (yourMessage === "") {
+    inputMessage.style.border = "solid 1px red";
+    return alert`Please tell us what you need`;
+  }
+  inputMessage.style.border = "";
 
-alert(`hey ${yourName} we will get back to you !!!`)
+  alert(`hey ${yourName} we will get back to you !!!`);
 
-inputEmail.value = ""
-inputMessage.value = ""
-chooseTopic.value = ""
-firstName.value = ""
+  inputEmail.value = "";
+  inputMessage.value = "";
+  chooseTopic.value = "";
+  firstName.value = "";
 
-
-return 
+  return;
 });
-
